d8f5a5358bb54d3240f3230256c1347230bd52dd
yongjuncha <perfect_yon@naver.com>
2022-01-27 14:45:29 +0900

    Initial Commit

---
+++ LimitTimerTest/LimitTimerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimitTimerTestApp.swift
+//  LimitTimerTest
+//
+//  Created by Yong Jun Cha on 2022/01/27.
+//
+
+import SwiftUI
+
+@main
+struct LimitTimerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
